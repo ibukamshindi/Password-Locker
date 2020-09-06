ab961f714fedcfd3c3455db2fc9c969fc8f06525
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 21:20:55 +0300

    add test to check if credentials have intialised

--- credentials_test.py
+++ credentials_test.py
@@ -2,4 +2,21 @@ import unittest
 from credentials import Credentials
 
 class TestCredentials(unittest.TestCase) :
-  
+    """
+    Test class that defines test cases for the credentials behaviours
+    """
+    def setUp(self):
+        """
+        Set up method to run before each test case 
+        """
+        self.new_credentials = Credentials("Twitter", "12345")
+
+    def test_init(self) :
+        """
+        test_init test case to test if the object is initialised properly
+        """
+        self.assertEqual(self.new_credentials.account_name,"Twitter")
+        self.assertEqual(self.new_credentials.account_password,"12345")
+        
+if __name__ == "__main__":
+      unittest.main()
