227b436a547284e8cfcda0ad57a2f78f70c905ab
ibukamshindi <ibukamshindi@gmail.com>
2020-09-07 00:03:56 +0300

    add functions for creating users and saving users

--- run.py
+++ run.py
@@ -3,5 +3,17 @@ import random
 from user import User
 from credentials import Credentials
 
+def create_user(username,password):
+    """
+    Function to create a new user
+    """
+    new_user = User(username, password)
+    return new_user
+
+def save_user(user):
+    """
+    function to save user 
+    """
+    user.save_user()    
 
 
