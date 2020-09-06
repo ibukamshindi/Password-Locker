bc9a8003095bc23746ab89dcb6ae4804f1754814
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 18:26:35 +0300

    add init method to class credentials

--- credentials.py
+++ credentials.py
@@ -2,3 +2,6 @@ class Credentials:
   """
   Class that creates new instances of credentials 
   """
+  def __init__(self,account_name,account_password):
+      self.account_name = account_name
+      self.account_password = account_password
