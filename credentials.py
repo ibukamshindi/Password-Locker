b6ef1f79a935960e63847f090233ce841e67a56d
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 18:42:22 +0300

    import unit test and import class credentials

--- credentials.py
+++ credentials.py
@@ -5,3 +5,5 @@ class Credentials:
   def __init__(self,account_name,account_password):
       self.account_name = account_name
       self.account_password = account_password
+  credentials_list = []
+  
