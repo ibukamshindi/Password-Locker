ee0b06b0b280c26db7bd89458cc20101832f6c35
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 15:54:13 +0300

    add setUp Method and test if user are instantied properly

--- user_test.py
+++ user_test.py
@@ -10,3 +10,17 @@ class TestUser(unittest.TestCase) :
          """
          Set up method to run before each test case 
          """
+         self.new_user = User("Juma","12345")
+    
+    def test_init(self) :
+         """
+         test_init test case to test if the object is initialised properly
+         """
+         self.assertEqual(self.new_user.user_name,"Juma")
+         self.assertEqual(self.new_user.password,"12345")
+    
+
+if __name__ == "__main__":
+      unittest.main()
+
+
