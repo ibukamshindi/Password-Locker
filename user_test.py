51365eb9b3349707d8fd33ca4224877f7937d100
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 15:28:38 +0300

    add import unit test user fromUser create subclass TestUser

---
+++ user_test.py
@@ -0,0 +1,12 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase) :
+
+    """
+    Test class that defines test cases for the user class behaviours
+    """
+    def setUp(self):
+         """
+         Set up method to run before each test case 
+         """
