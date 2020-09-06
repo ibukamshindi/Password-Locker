958f2d8fef71021960dd15e34ffd71d8d03ee97a
ibukamshindi <ibukamshindi@gmail.com>
2020-09-06 13:01:42 +0300

    add user_list to create user objects

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
   """
   Class that generates new instances of users
   """
+  user_list = []
+
   def __init__(self,user_name,password) :
      self.user_name = user_name
      self.password = password
